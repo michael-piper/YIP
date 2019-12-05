a3abacb2c19181d7054aaeb7274f6fe727bc8d93
michael piper <pipermichael@aol.com>
2019-12-05 09:06:07 +0100

    editproduct updated

--- resources/views/screen/contactus.blade.php
+++ resources/views/screen/contactus.blade.php
@@ -8,7 +8,7 @@
 @endsection
 
 @section('js')
-
+<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="f4594748-710e-483a-bc2e-f3a9c02254a6";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
 @endsection
 
 @section('content')
